bea3e8593518dd007cc3fe58a7dad91c49253aac
Ananya Bahadur CB.EN.U4CSE13106 <foss@foss.amritanet.edu>
2017-02-22 16:18:37 +0530

    production config change

--- application/models/Login.php
+++ application/models/Login.php
@@ -1,22 +1,12 @@
 <?php
 class login extends CI_Model{
-    function __construct(){
-        $this->load->database();
-    }
-    function ulogin($tname,$pass){
-
+    function ulogin($tid,$pass){
+       $this->load->database();
        $this->db->select("*");
        $this->db->from("teams");
-       $this->db->where("name",$tname);
+       $this->db->where("id",$tid);
        $this->db->where("passwd",$pass);
        $udata = $this->db->get();
        return $udata->row_array();
-    }
-    function getfext($tid){
-        $this->db->select("platform");
-        $this->db->from("teams");
-        $this->db->where("id",$tid);
-        $udata = $this->db->get();
-        return $udata->row_array();
     }
 }
