45a7b465c262ba7d023624f0474e6af552304924
kogul <coder30597@gmail.com>
2017-03-01 16:12:59 +0530

    Leaderboard logic

--- application/models/leader.php
+++ application/models/leader.php
@@ -5,7 +5,7 @@ class leader extends CI_Model{
         $this->load->database();
         $this->db->select("*");
         $this->db->from("teams");
-        $this->db->order_by("rank","desc");
+        $this->db->order_by("rank");
         $scores = $this->db->get();
         return $scores->result();
     }
